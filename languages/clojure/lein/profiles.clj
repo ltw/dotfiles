77c8fb7440729bed54ec698d7f560f30324797fa
Lucas Willett <me@ltw.io>
2014-08-02 21:53:20 -0500

    Add lein-try and gary's usage graph.

--- languages/clojure/lein/profiles.clj
+++ languages/clojure/lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[lein-marginalia "0.7.1"]
                   [cider/cider-nrepl "0.7.0-SNAPSHOT"]
+                  [lein-try "0.4.1"]
                   [lein-clojars "0.9.1"]
                   [lein-ancient  "0.5.5"]
-                  [lein-pprint "1.1.1"]]}}
+                  [lein-pprint "1.1.1"]]
+        :dependencies [[com.gfredericks/clj-usage-graph "0.1.3"]] }}
