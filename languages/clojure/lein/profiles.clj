60fabcb7b24e9b63dc86132836981d4dc50a4e34
Lucas Willett <me@ltw.io>
2014-10-09 13:17:41 -0500

    Add more stuff to my .lein.

--- languages/clojure/lein/profiles.clj
+++ languages/clojure/lein/profiles.clj
@@ -3,5 +3,7 @@
                   [lein-try "0.4.1"]
                   [lein-clojars "0.9.1"]
                   [lein-ancient  "0.5.5"]
+                  [lein-exec "0.3.4"]
                   [lein-pprint "1.1.1"]]
-        :dependencies [[com.gfredericks/clj-usage-graph "0.1.3"]] }}
+        :dependencies [[com.gfredericks/clj-usage-graph "0.1.3"]
+                       [org.clojure/tools.trace "0.7.8"]]}}
